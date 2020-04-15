49282e0cf1aa48ba7cb182b35426051ac618d3a1
unknown <nkosimova06@gmail.com>
2020-04-15 20:47:47 +0500

    making connection with DB

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PersonTableSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conString  = @"Data source=KosimovaNodira; Initial catalog = Alif_Academy; Integrated Security=True"; 
+            SqlConnection con = new SqlConnection(conString);
+
+            con.Open();//open connection 
+            
+            string commandText = "Select * from Person";
+            SqlCommand command = new SqlCommand(commandText,con);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"ID:{reader.GetValue(0)}, FirstName: {reader.GetValue(1)},LastName: {reader.GetValue(2)}");
+            }
+
+
+        }
+    }
+}
